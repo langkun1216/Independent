19c58c21534e99891f3a170f7a783b8e9c5aa2f7
langkun <kun.lang@inzymeits.com>
2019-01-15 15:01:31 +0800

    修改业务

--- src/main/java/com/independent/independent/Test.java
+++ src/main/java/com/independent/independent/Test.java
@@ -1,23 +1,10 @@
 package com.independent.independent;
 
 import com.independent.independent.business.OvertimePay;
-import com.independent.independent.model.OvertimePayModel;
-import com.independent.independent.utils.DateUtil;
-import com.independent.independent.utils.ExcelReaderUtil;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.hssf.usermodel.*;
 
 import javax.swing.*;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 public class Test {
-
-
 
     public static void main(String[] args){
         Object[] possibleValues = {"导出加班费"};
@@ -29,6 +16,5 @@ public class Test {
             new OvertimePay().getOvertimePayByExcel(path);
         }
     }
-
 
 }
